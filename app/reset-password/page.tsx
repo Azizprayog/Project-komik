e64642c53c3812c4b2cb9e2fd53a76364cbcb973
Aziz <abdulprayoga12@gmail.com>
2026-01-26 14:57:29 +0700

    fix

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -2,6 +2,8 @@ import { Suspense } from "react";
 import ResetPasswordClient from "./ResetPasswordClient";
 
 export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
 
 export default function Page() {
   return (
